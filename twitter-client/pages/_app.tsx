ddb427579ad2edff55de2ceefbef96abfab3c7e4
yashmandi <yashmandi18@gmail.com>
2024-05-27 18:49:44 +0530

    client

--- twitter-client/pages/_app.tsx
+++ twitter-client/pages/_app.tsx
@@ -1,17 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
-import {GoogleOAuthProvider} from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import toast, { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
-  <div  className={inter.className}>
-  <GoogleOAuthProvider clientId="759411181092-r46lseqhhmhuqdlfrkgdubennet90u6b.apps.googleusercontent.com">
-    <Component {...pageProps} />;
-  </GoogleOAuthProvider>
+    <div className={inter.className}>
+      <GoogleOAuthProvider clientId="759411181092-r46lseqhhmhuqdlfrkgdubennet90u6b.apps.googleusercontent.com">
+        <Component {...pageProps} />;
+        <Toaster />
+      </GoogleOAuthProvider>
     </div>
   </div>
 }
--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -3,31 +3,31 @@ import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
 import { BsBell, BsBookmark, BsEnvelope } from "react-icons/bs";
 import { FaXTwitter } from "react-icons/fa6";
 import { SlOptions } from "react-icons/sl";
-import {GoogleLogin} from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 import { useCallback } from "react";
+import { toast } from "react-hot-toast";
 
 interface TwitterSidebarButton {
   title: string,
   icon: React.ReactNode;
-  
 }
 
-const sidebarMenuItems : TwitterSidebarButton [] =[
+const sidebarMenuItems: TwitterSidebarButton[] = [
   {
     title: 'Home',
-    icon: <BiHomeCircle/>
+    icon: <BiHomeCircle />
   },
   {
     title: 'Explore',
-    icon: <BiHash/>
+    icon: <BiHash />
   },
   {
     title: 'Notifications',
-    icon: <BsBell/>
+    icon: <BsBell />
   },
   {
     title: 'Messages',
-    icon: <BsEnvelope/>
+    icon: <BsEnvelope />
   },
   {
     title: 'Bookmarks',
@@ -35,57 +35,83 @@ const sidebarMenuItems : TwitterSidebarButton [] =[
   },
   {
     title: 'Profile',
-    icon: <BiUser/>
+    icon: <BiUser />
   },
-  {  
+  {
     title: 'More',
-    icon: <SlOptions/> 
+    icon: <SlOptions />
   }
 ]
 
 export default function Home() {
+  const handleLoginWithGoogle = useCallback(async (cred: any) => {
+    const googleToken = cred.credential;
+    if (!googleToken) {
+      toast.error(`Google token not found`);
+      return;
+    }
 
-  const handleLoginWithGoogle = useCallback((cred: Credential) => {}, [])
+    try {
+      const response = await fetch('/verify-google-token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: googleToken }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to verify token');
+      }
+
+      const data = await response.json();
+      toast.success("Verified Success");
+      console.log(data);
+    } catch (error) {
+      console.error("Error verifying Google token:", error);
+      toast.error("Failed to verify Google token");
+    }
+  }, []);
 
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
         <div className="col-span-3 pt-3 pl-14 ">
-          <div className="text-3xl hover:bg-gray-900 rounded-full p-1 ml-3  h-fit w-fit cursor-pointer transition-all">
-          <FaXTwitter />
+          <div className="text-3xl hover:bg-gray-900 rounded-full p-1 ml-3 h-fit w-fit cursor-pointer transition-all">
+            <FaXTwitter />
           </div>
           <div className="mt-6 text-xl font-semibold">
             <ul>
-            {sidebarMenuItems.map(item =>
-            <li
-              className="flex justify-start items-center gap-4 hover:bg-gray-900 rounded-full px-5 py-2 w-fit cursor-pointer transition-all mt-2"
-              key={item.title}
-            >
-              <span>{item.icon}</span>
-              <span>{item.title}</span>
-            </li>
-            )}
+              {sidebarMenuItems.map(item =>
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-900 rounded-full px-5 py-2 w-fit cursor-pointer transition-all mt-2"
+                  key={item.title}
+                >
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              )}
             </ul>
             <button className="bg-[#1d9bf0] p-2 rounded-full w-52 mt-5">Tweet</button>
           </div>
         </div>
         <div className="col-span-6 border-r-[0.1px] border-l-[0.2px] h-screen overflow-x-auto border-gray-700">
-          <FeedCard/>
-          <FeedCard/>
-          <FeedCard/>
-          <FeedCard/> 
-          <FeedCard/>
-          <FeedCard/>
-          <FeedCard/>
-          <FeedCard/>
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
         <div className="col-span-3">
-          <div className=" p-5 bg-slate-700 rounded-lg ">  
+          <div className="p-5 bg-slate-700 rounded-lg">
             <h1 className="text-2xl font-semibold my-2">New to X?</h1>
-            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+            <GoogleLogin onSuccess={handleLoginWithGoogle} />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
